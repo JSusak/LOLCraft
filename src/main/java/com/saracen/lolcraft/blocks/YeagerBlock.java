b7fda9c3136c58ec9317ca2f93e26747173c3257
Josh <joshuasusak@gmail.com>
2022-01-30 22:35:39 +0000

    update strength

--- src/main/java/com/saracen/lolcraft/blocks/YeagerBlock.java
+++ src/main/java/com/saracen/lolcraft/blocks/YeagerBlock.java
@@ -7,7 +7,7 @@ import net.minecraft.world.level.material.Material;
 public class YeagerBlock extends Block {
     public YeagerBlock() {
         super(Block.Properties.of(Material.GLASS)
-                .strength(3.0f, 5.0f)
+                .strength(2.0f, 5.0f)
                 .sound(SoundType.GLASS)
                 .requiresCorrectToolForDrops()
 
